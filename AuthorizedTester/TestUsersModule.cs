8a392107ad325e1a37ed7f21174233f3a46b3b18
Steve Vaneeckhout <s.vaneeckhout@nexwork.nl>
2020-07-13 15:14:41 +0200

    Add Domains blacklist and Module Enabled setting

--- AuthorizedTester/TestUsersModule.cs
+++ AuthorizedTester/TestUsersModule.cs
@@ -1,7 +1,8 @@
-﻿// Based on https://www.asp.net/web-api/overview/security/basic-authentication
+﻿// Based on https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/basic-authentication
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Web;
@@ -12,19 +13,38 @@ namespace AuthorizedTester
     public class TestUsersModule : IHttpModule
     {
         private const string Realm = "TestEnvironment";
+        private bool ModuleEnabled { get; set; }
         private List<string> IPWhitelist { get; set; }
         private List<string> UserWhitelist { get; set; }
+        private List<string> DomainsBlacklist { get; set; }
 
         public void Dispose() { }
 
         public void Init(HttpApplication application)
         {
             LoadSettings();
-            application.BeginRequest += Application_BeginRequest;
+
+            if (ModuleEnabled)
+            {
+                application.BeginRequest += Application_BeginRequest;
+            }
         }
 
         private void LoadSettings()
         {
+            var module = WebConfigurationManager.AppSettings["TestUsersModule.Enabled"];
+            if (module != null && bool.TryParse(module, out bool enableModule))
+            {
+                ModuleEnabled = enableModule;
+            }
+            else
+            {
+                // Setting not found or invalid value, turn it on anyway
+                ModuleEnabled = true;
+            }
+
+            if (!ModuleEnabled) return; // No need to read other settings if the module is not turned on.
+
             var ips = WebConfigurationManager.AppSettings["TestUsersModule.Whitelist.IP"];
             if (ips != null)
             {
@@ -36,6 +56,12 @@ namespace AuthorizedTester
             {
                 UserWhitelist = new List<string>(users.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
             }
+
+            var domains = WebConfigurationManager.AppSettings["TestUsersModule.Blacklist.Domains"];
+            if (domains != null)
+            {
+                DomainsBlacklist = new List<string>(domains.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
+            }
         }
 
         private void Application_BeginRequest(object sender, EventArgs e)
@@ -44,47 +70,50 @@ namespace AuthorizedTester
             HttpRequest request = application.Context.Request;
             HttpResponse response = application.Context.Response;
 
-            if (IPWhitelist != null && IPWhitelist.Contains(request.UserHostAddress))
+            if (DomainsBlacklist == null || !DomainsBlacklist.Any() || (request.Url.HostNameType == UriHostNameType.Dns && DomainsBlacklist.Contains(request.Url.Host)))
             {
-                // IP found, do nothing
-                response.Headers.Add("X-TestUser", "Access by IP");
-                return;
-            }
-
-            if (UserWhitelist != null)
-            {
-                string authHeader = request.Headers["Authorization"];
-
-                if (authHeader != null)
+                if (IPWhitelist != null && IPWhitelist.Contains(request.UserHostAddress))
                 {
-                    var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+                    // IP found, do nothing
+                    response.Headers.Add("X-TestUser", "Access by IP");
+                    return;
+                }
 
-                    // RFC 2617 sec 1.2, "scheme" name is case-insensitive
-                    if (authHeaderVal.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) && authHeaderVal.Parameter != null)
+                if (UserWhitelist != null)
+                {
+                    string authHeader = request.Headers["Authorization"];
+
+                    if (authHeader != null)
                     {
-                        if (DoesUserExist(authHeaderVal.Parameter))
+                        var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+
+                        // RFC 2617 sec 1.2, "scheme" name is case-insensitive
+                        if (authHeaderVal.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) && authHeaderVal.Parameter != null)
                         {
-                            // User is valid, do nothing
-                            response.Headers.Add("X-TestUser", "Access by credentials");
-                            return;
+                            if (DoesUserExist(authHeaderVal.Parameter))
+                            {
+                                // User is valid, do nothing
+                                response.Headers.Add("X-TestUser", "Access by credentials");
+                                return;
+                            }
                         }
                     }
+                    else
+                    {
+                        // No authenticationheader found, request a username and password
+                        response.Clear();
+                        response.StatusCode = 401;
+                        response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
+                        response.End();
+                    }
                 }
-                else
-                {
-                    // No authenticationheader found, request a username and password
-                    response.Clear();
-                    response.StatusCode = 401;
-                    response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
-                    response.End();
-                }
-            }
 
-            // No whitelisted IP or user found, block access
-            response.Clear();
-            response.StatusCode = 403;
-            response.StatusDescription = "Forbidden";
-            response.End();
+                // No whitelisted IP or user found, block access
+                response.Clear();
+                response.StatusCode = 403;
+                response.StatusDescription = "Forbidden";
+                response.End();
+            }
         }
 
         private bool DoesUserExist(string credentials)
